e30de6c4ab99be00b6823cb79791b23243ee7cbc
Reinald Santos <reinaldsantos312@gmail.com>
2026-01-29 10:15:17 +0000

    DEPLOY: Configuração completa para Render.com | Database URL, server config, plugins, render.yaml

--- config/database.js
+++ config/database.js
@@ -2,14 +2,8 @@
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL', true) ? {
-        rejectUnauthorized: false
-      } : false,
+      connectionString: env('DATABASE_URL'),
+      ssl: { rejectUnauthorized: false }
     },
     pool: {
       min: 0,
